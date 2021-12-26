54134c4335fb43c9fca2d23f1c0585afca91ff8c
Lucio Cabrera <Lucio.Cabrera@Just-BI.nl>
2021-12-26 16:42:04 +0100

    issue-4 Implement proptypes for the auto complete connected component

---
+++ src/components/AutoComplete/AutoCompleteConnected/AutoCompleteConnected.propTypes.js
@@ -0,0 +1,6 @@
+import PropTypes from 'prop-types';
+
+export const autoCompleteConnectedPropTypes = {
+  /** Specifies the method to get the url for fetching the data */
+  getFetchUrl: PropTypes.func.isRequired
+};
